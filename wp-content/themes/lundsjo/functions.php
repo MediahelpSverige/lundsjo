9a7b86ec2e3f7b05159a0ff35190df3eee48f1bc
alienwizard <johanwendin@gmail.com>
2016-11-17 17:12:08 +0100

    fixed grid performance

--- wp-content/themes/lundsjo/functions.php
+++ wp-content/themes/lundsjo/functions.php
@@ -57,7 +57,7 @@ function my_enqueue_assets() {
 
     wp_enqueue_script( 'script',  get_template_directory_uri() . '/js/script.js', array( 'jquery' ), NULL, true);
 
-  wp_enqueue_script( 'bundle',  get_template_directory_uri() . 'bundle.js', array( 'jquery' ), NULL, true);
+  wp_enqueue_script( 'bundle',  get_template_directory_uri() . '/bundle.js', array( 'jquery' ), NULL, true);
 
     global $wp_query;
 
