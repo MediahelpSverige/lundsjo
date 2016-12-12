c69b8a74f6954b0e53228f4ed920fa2c4b2ed616
alienwizard <johanwendin@gmail.com>
2016-12-12 13:51:58 +0100

    changed image size

--- wp-content/themes/lundsjo/src/js/fp-scripts.js
+++ wp-content/themes/lundsjo/src/js/fp-scripts.js
@@ -351,7 +351,7 @@ var grid;
 
 
 
-                    $('.grid_canvas').append('<div class="project-item animated zoomIn"><a href="'+ data[i].link +'"><img src="'+data[i].better_featured_image.source_url+'"> <div class="title-bak"><h3 class="project-text">'+ data[i].title.rendered +'</h3></div></a></div>');
+                    $('.grid_canvas').append('<div class="project-item animated zoomIn"><a href="'+ data[i].link +'"><img src="'+data[i].better_featured_image.source_url.media_details.medium_large+'"> <div class="title-bak"><h3 class="project-text">'+ data[i].title.rendered +'</h3></div></a></div>');
 
 
                     imagesLoaded( document.querySelector('.grid_canvas'), function( instance ) {
@@ -389,7 +389,7 @@ var grid;
 													console.log(data);
 
 													for (var i = 0; i < data.length; i++) {
-														console.log(data[i].better_featured_image.source_url);
+													
 
 														$('.grid_canvas').append('<div class="project-item animated zoomIn"><a href="'+ data[i].link +'"><img src="'+data[i].better_featured_image.source_url.media_details.medium_large+'"> <div class="title-bak"><h3 class="project-text">'+ data[i].title.rendered +'</h3></div></a></div>');
 
